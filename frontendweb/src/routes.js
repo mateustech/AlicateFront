9cffb4bc8fe8f0bd7dbf20ad03267517942a084f
mateustech <mateustech686@gmail.com>
2020-02-11 22:41:07 -0300

    Components

---
+++ frontendweb/src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import SignUp from './pages/SignUp/signUp'
+import Sign from './pages/Sign/sign'
+import Main from './pages/Main/main'
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/signup" component={SignUp}></Route>
+        <Route exact path="/sign" component={Sign}></Route>
+        <Route exact path="/" component={Sign}></Route>
+      </Switch>
+    </BrowserRouter>
+  )
+}
