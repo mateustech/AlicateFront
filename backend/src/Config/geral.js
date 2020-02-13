3f6cb65481cfeba2cc46d494e937c8e79ff7c70c
mateustech <mateustech686@gmail.com>
2020-02-12 21:11:52 -0300

    First Commit

--- backend/src/Config/geral.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  PORT: 3333,
-  CONNECT: 'mongodb+srv://mdev:megahack@cluster0-9xwvg.mongodb.net/test?retryWrites=true&w=majority'
-}
--- backend/src/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const routes = express.Router();
-
-routes.get('/', (req, res) => {
-  return res.status(200).json({ message: "Este Serviço esta Funcionando com Sucesso" })
-})
-
-module.exports = routes;
