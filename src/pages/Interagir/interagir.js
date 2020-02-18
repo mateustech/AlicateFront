da770ac98fbb89d4488a20364ced3acd0ce027be
mateustech <mateustech686@gmail.com>
2020-02-18 09:37:03 -0300

    changes

--- src/pages/Interagir/interagir.js
+++ src/pages/Interagir/interagir.js
@@ -1,19 +1,9 @@
 import React from 'react';
 
 // import { Container } from './styles';
-import './interagir.css'
-import im1 from '../../assents/bginteract1.png'
+
 export default function Interagir() {
   return (
-    <React.Fragment>
-      <div className="interact">
-        <img src={im1} alt="" />
-        <h1>asd</h1>
-      </div>
-      <div className="interact1">
-        <h1>asd</h1>
-      </div>
-
-    </React.Fragment>
+    <div />
   );
 }
